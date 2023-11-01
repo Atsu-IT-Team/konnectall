069e77ec53e6f1e15ea6dd8e3a16fe6faf6dc28b
Ajay Chauhan <chauhan.ajay1791@gmail.com>
2023-11-01 13:48:55 +0530

    Write custom table creat script

--- Presentation/Nop.Web/App_Data/SQLQuery.sql
+++ Presentation/Nop.Web/App_Data/SQLQuery.sql
@@ -29,3 +29,47 @@ BEGIN
 	CONSTRAINT IsMostPopular_Default_Standard DEFAULT 0
 	WITH VALUES
 END
+
+--Changes on 01-11-2023
+
+GO
+/****** Object:  Table [dbo].[KA_ApplicationDocuments]    Script Date: 01-11-2023 13:04:52 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[KA_ApplicationDocuments](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[ApplicationId] [int] NULL,
+	[DocumentName] [nvarchar](max) NULL,
+ CONSTRAINT [PK_KA_ApplicationDocuments] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[KA_ApplicationRequest]    Script Date: 01-11-2023 13:04:52 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[KA_ApplicationRequest](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[FirstName] [nvarchar](200) NOT NULL,
+	[LastName] [nvarchar](200) NOT NULL,
+	[Email] [nvarchar](300) NOT NULL,
+	[Phone] [nvarchar](50) NOT NULL,
+	[Resume] [nvarchar](200) NULL,
+	[CreatedOnUtc] [datetime2](7) NOT NULL,
+ CONSTRAINT [PK_KA_ApplicationRequest] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[KA_ApplicationDocuments]  WITH CHECK ADD  CONSTRAINT [FK_KA_ApplicationDocuments_ApplicationId_KA_ApplicationRequest_Id] FOREIGN KEY([ApplicationId])
+REFERENCES [dbo].[KA_ApplicationRequest] ([Id])
+GO
+ALTER TABLE [dbo].[KA_ApplicationDocuments] CHECK CONSTRAINT [FK_KA_ApplicationDocuments_ApplicationId_KA_ApplicationRequest_Id]
+GO
+--Changes on 01-11-2023
